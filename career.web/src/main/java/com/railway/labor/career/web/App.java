64698a7b8d8d2d1d3872bbbe9211547592df9450
zhuanglx <zhlx012@163.com>
2018-03-13 23:29:42 +0800

    create project
    create project

---
+++ career.web/src/main/java/com/railway/labor/career/web/App.java
@@ -0,0 +1,13 @@
+package com.railway.labor.career.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
